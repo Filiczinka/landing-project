13ef399d3a9581ef8376989aa983d0c5382b10ca
Filiczinka <filiczinka@gmail.com>
2023-03-08 22:34:04 +0200

    changed js

--- scripts/heade-banner.js
+++ scripts/heade-banner.js
@@ -4,9 +4,13 @@ const words = ['Extensive', 'Adjective', 'Complete'];
 let currentIndex = 0;
 
 
-function changeTitle() {
+setInterval() => {
   titleElement.textContent = words[currentIndex];
   currentIndex = (currentIndex + 1) % words.length;
-}
+}, 2000);
 
 setInterval(changeTitle, 2000);
+
+const synonyms = ['TASTY', 'YUMMY', 'SPICY'];
+const headerBannerSynonyms = document.querySelector('.header__banner-synonyms');
+let currentIndex = 0;
