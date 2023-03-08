94b5c7336267ba2970c6239cf7cd73b4497e5a6e
Filiczinka <filiczinka@gmail.com>
2023-03-08 23:37:36 +0200

    delete js

--- scripts/header-banner.js
+++
@@ -1,9 +0,0 @@
-const words = ['Extensive', 'Adjective', 'Complete'];
-const titleElement = document.querySelector('.header__change-title');
-
-let currentIndex = 0;
-
-setInterval(() => {
-  titleElement.textContent = words[currentIndex];
-  currentIndex = (currentIndex + 1) % words.length;
-}, 2000);
